3f3d01fb039867f4ddaab1d879a49df9979393ba
arjunnpndt <arjun.pandit@knoldus.com>
2023-12-27 23:30:34 +0530

    validation added

--- script.ts
+++ script.ts
@@ -1,14 +1,26 @@
 interface User {
   username: string | null;
   githubid: string | null;
-  gender: string | null;
-  country: string | null;
 }
 
 const loadingTime = (delaytime: number): Promise<void> => {
   return new Promise((resolve) => setTimeout(resolve, delaytime));
 };
 
+const validateForm = (user: User): Promise<void> => {
+  // creating a promise to validate the form
+  return new Promise((resolve, reject) => {
+    if (user.username === "" || user.githubid === "") {
+      // according to your validation logic here you can reject the promise can throw an error
+      reject(new Error("Please fill all the fields"));
+    } else {
+      // if the validation is successful you can resolve the promise
+      resolve();
+    }
+  });
+};
+
+//
 const loading = async (): Promise<void> => {
   console.log("loading...");
   await loadingTime(3000);
@@ -21,30 +33,20 @@ const submitForm = async (): Promise<any> => {
   try {
     console.log((<HTMLInputElement>document.getElementById("userName")).value);
 
-    // const userData: User = {
-    //   username: (<HTMLInputElement>document.getElementById("userName")).value,
-    //   githubid: (<HTMLInputElement>document.getElementById("githubID")).value,
-    //   gender: (<NodeListOf<HTMLInputElement>>(
-    //     document.getElementsByName("gender")
-    //   )).value,
-    //   country: (<HTMLSelectElement>document.getElementById("country")).value,
-    // };
+    const userData: User = {
+      username: (<HTMLInputElement>document.getElementById("userName")).value,
+      githubid: (<HTMLInputElement>document.getElementById("githubID")).value,
+    };
 
-    // console.log(userData.username);
+    // using this to show how to use async await
+    const validatation = await validateForm(userData);
 
-    const response = await fetch(
-      "https://jsonplaceholder.typicode.com/todos/1"
-    );
-    if (!response.ok) {
-      throw new Error("Something went wrong");
-    }
-    // console.log("before loading");
-    // await loadingTime(3000);
-    // console.log("after loading");
-    // it's just to show how catch block works
-    // setTimeout(() => {
-    //   throw new Error("Chill , it's just a demo");
-    // }, 3000);
+    // const response = await fetch(
+    //   "https://jsonplaceholder.typicode.com/todos/1"
+    // );
+    // if (!response.ok) {
+    //   throw new Error("Something went wrong");
+    // }
   } catch (error) {
     console.log(error);
   }
